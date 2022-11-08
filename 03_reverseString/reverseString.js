fbd16dfcf142f2cafbdd8b2bb935db18c6fe8df1
abel <abel_perez9@hotmail.com>
2022-11-08 16:42:35 -0600

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
+const reverseString = string => string.split('').reverse().join('');
 
-};
-
+reverseString('hello there');
 // Do not edit below this line
 module.exports = reverseString;
