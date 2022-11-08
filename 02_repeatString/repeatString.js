7278447fcc6974fc3bade272218bd9e598216105
abel <abel_perez9@hotmail.com>
2022-11-08 15:57:48 -0600

    02 lesson finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+    if (num < 0) return 'ERROR';
+    let word = '';
+    for (let i = 0; i < num; i++) {
+        word += string
+    }
+    return word;
+}
 
-};
-
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('', 10);
 // Do not edit below this line
 module.exports = repeatString;
