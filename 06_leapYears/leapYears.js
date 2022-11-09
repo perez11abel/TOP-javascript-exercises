9958b02555eb10ce015fdd199e913d9b81816efb
abel <abel_perez9@hotmail.com>
2022-11-09 15:45:08 -0600

    06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
-const leapYears = function() {
+const leapYears = year => year % 4 === 0 && ( year % 100 !==0 || year % 400 ===0);
 
-};
-
+leapYears(2000) // is a leap year: returns true
+leapYears(1985) // is not a leap year: returns false
 // Do not edit below this line
 module.exports = leapYears;
