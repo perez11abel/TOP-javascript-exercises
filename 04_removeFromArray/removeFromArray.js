01e3e66813d80a171f10d0db35c9e17521bbceaa
abel <abel_perez9@hotmail.com>
2022-11-08 19:59:52 -0600

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = (...args) => {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
-};
-
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
