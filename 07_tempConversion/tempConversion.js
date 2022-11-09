4db97d67590e8232b61c75a562c1298a313faf54
abel <abel_perez9@hotmail.com>
2022-11-09 16:00:25 -0600

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round((fahrenheit-32) / 1.8);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return Math.round((celsius*1.8)+32);
 };
+ftoc(32) // fahrenheit to celsius, should return 0
 
+ctof(0) // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   ftoc,
