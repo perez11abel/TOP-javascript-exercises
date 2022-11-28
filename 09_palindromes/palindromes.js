964274b39ab18d69659a6949113d8f55162db9dc
abel <abel_perez9@hotmail.com>
2022-11-28 10:39:40 -0600

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function(string) {
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
+  };
+palindromes('racecar');
+palindromes('Racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
+
 module.exports = palindromes;
