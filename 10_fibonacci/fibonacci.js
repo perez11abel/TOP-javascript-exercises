72c24f0fedf687bced1bd5743de2422902b023f8
abel <abel_perez9@hotmail.com>
2022-11-28 12:04:05 -0600

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < count; i++) {
+      const temp = b;
+      b = a + b;
+      a = temp;
+    }
+    return b;
+  };
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(-25);
+fibonacci('1');
+fibonacci('2');
+fibonacci('8');
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
