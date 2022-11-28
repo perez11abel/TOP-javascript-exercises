5de1dd85da110af3c7a324000e6887fa7fc8d9d7
abel <abel_perez9@hotmail.com>
2022-11-28 10:11:48 -0600

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
-};
+const add = (a,b) => a + b;
+add(0,0);
+add(2,2);
+add(2,6);
 
-const subtract = function() {
-	
-};
+const subtract = (a,b) => a - b;
+subtract(10,4);
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
+sum([]);
+sum([7]);
+sum([7, 11]);
+sum([1,3,5,7,9]);
 
-const multiply = function() {
-
+const multiply = function(array) {
+ return array.length
+  ? array.reduce((total, current) => total * current)
+    : 0;
 };
+multiply([2,4]);
+multiply([2,4,6,8,10,12,14]);
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
+power(4,3);
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+factorial(0);
+factorial(1);
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
